6d53355d5fa87cef9b1fa48b8ba655d81a512631
Sprocket <24927632+Sprocket06@users.noreply.github.com>
2021-08-25 10:35:34 -0400

    initial commit

---
+++ Momentum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Chroma;
+
+namespace Momentum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new GameCore();
+            g.Run();
+        }
+    }
+}
